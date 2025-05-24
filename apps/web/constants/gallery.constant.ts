19247cf803445eadc6bf5d78993c9204b552eed6
Dijalma Silva <dijalmacz@gmail.com>
2025-05-24 02:19:03 -0300

    fix(react): create a react project instead a NextJS

--- apps/web/constants/gallery.constant.ts
+++
@@ -1,2 +0,0 @@
-export const QTD_ITEMS = 15
-export const ARTWORKS_KEY_QUERY = 'artworks'
--- apps/web/constants/met-museum.constant.ts
+++
@@ -1,6 +0,0 @@
-export const MUSEUM_API_URL = 'https://collectionapi.metmuseum.org/'
-export const ARTS_WITH_IMAGES = `${MUSEUM_API_URL}/public/collection/v1/search?hasImages=true&q=painting`
-export const ARTWORKS = `${MUSEUM_API_URL}/public/collection/v1/objects` // /{objectID} to get specific artwork
-export const ARTS_BY_ARTIST = `${MUSEUM_API_URL}/public/collection/v1/search?artistOrCulture=true&q=` // van+gogh use this pattern to search by artists
-export const DEPARTMENTS = `${MUSEUM_API_URL}/public/collection/v1/departments`
-export const ARTS_BY_DEPARTMENT = `${MUSEUM_API_URL}/public/collection/v1/search?departmentId=` // 11&q=portrait use this pattern to search by department
