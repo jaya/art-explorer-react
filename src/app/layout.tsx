fd756c6cd68318add0ab1293e860d926c3c2cf1b
Adeonir Kohl <adeonir@gmail.com>
2025-05-27 12:08:26 -0300

    Add google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,25 @@
 import '~/shared/styles/globals.css'
 
+import { Inria_Serif, Inter } from 'next/font/google'
 import type { ReactNode } from 'react'
+
+import { cn } from '~/shared/utils/className'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+const inriaSerif = Inria_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-inria-serif',
+})
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={cn(inter.variable, inriaSerif.variable, 'font-sans')}>{children}</body>
     </html>
   )
 }
