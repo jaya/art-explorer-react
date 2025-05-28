df86ff552a50261139a1bf46b4b8aff50ba6a57d
Adeonir Kohl <adeonir@gmail.com>
2025-05-28 11:35:39 -0300

    Update artworks hook to populate individual cache

--- src/modules/artworks/hooks/useArtworks.ts
+++ src/modules/artworks/hooks/useArtworks.ts
@@ -1,6 +1,6 @@
 'use client'
 
-import { useInfiniteQuery } from '@tanstack/react-query'
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
 
 import type { FetchArtworksResponse } from '~/modules/artworks/actions/fetchArtworks'
 import { fetchArtworks } from '~/modules/artworks/actions/fetchArtworks'
@@ -10,10 +10,20 @@ interface UseArtworksProps {
 }
 
 export function useArtworks({ initialData }: UseArtworksProps) {
+  const queryClient = useQueryClient()
+
   return useInfiniteQuery({
     queryKey: ['artworks'],
     queryFn: ({ pageParam = 1 }) => fetchArtworks({ page: pageParam }),
     getNextPageParam: (lastPage) => lastPage.nextPage,
+    select: (data) => {
+      data.pages.forEach((page) => {
+        page.data.forEach((artwork) => {
+          queryClient.setQueryData(['artwork', artwork.objectID], artwork)
+        })
+      })
+      return data
+    },
     initialPageParam: 1,
     initialData: {
       pages: [initialData],
