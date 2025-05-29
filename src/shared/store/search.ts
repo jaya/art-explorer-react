ac976b8c883e3153c85ac36dd843a7d7224f8ac3
Adeonir Kohl <adeonir@gmail.com>
2025-05-29 15:08:50 -0300

    Create search store

---
+++ src/shared/store/search.ts
@@ -0,0 +1,22 @@
+'use client'
+
+import { create } from 'zustand'
+
+interface SearchState {
+  query: string
+  isOpen: boolean
+}
+
+interface SearchActions {
+  setQuery: (query: string) => void
+  setIsOpen: (isOpen: boolean) => void
+}
+
+type SearchStore = SearchState & SearchActions
+
+export const useSearchStore = create<SearchStore>((set) => ({
+  query: '',
+  isOpen: false,
+  setQuery: (query) => set({ query }),
+  setIsOpen: (isOpen) => set({ isOpen }),
+}))
