91f51bba6ef987ec5942432d21910a1771ab17ba
Adeonir Kohl <adeonir@gmail.com>
2025-05-28 22:23:04 -0300

    Create favorites store and hook

---
+++ src/modules/favorites/store/favorite.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+import type { Artwork } from '~/modules/artworks/types'
+
+interface FavoriteState {
+  favorites: Artwork[]
+  isFavorite: (artwork: Artwork) => boolean
+}
+
+interface FavoriteActions {
+  addFavorite: (artwork: Artwork) => void
+  removeFavorite: (artwork: Artwork) => void
+}
+
+type FavoriteStore = FavoriteState & FavoriteActions
+
+export const useFavoriteStore = create<FavoriteStore>()(
+  persist(
+    (set, get) => ({
+      favorites: [],
+      isFavorite: (artwork) => get().favorites.some((favorite) => favorite.objectID === artwork.objectID),
+      addFavorite: (artwork) => set({ favorites: [...get().favorites, artwork] }),
+      removeFavorite: (artwork) =>
+        set({ favorites: get().favorites.filter((favorite) => favorite.objectID !== artwork.objectID) }),
+    }),
+    {
+      name: 'favorites',
+      skipHydration: true,
+    },
+  ),
+)
