eb17ad556a6021afd17734ec3a7700d565deefd3
Adeonir Kohl <adeonir@gmail.com>
2025-05-29 18:28:19 -0300

    Update header adding search to navigation menu

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,35 +1,24 @@
 'use client'
 
-import { ArrowRightIcon } from 'lucide-react'
-import { AnimatePresence, motion } from 'motion/react'
 import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
-import type { FormEvent } from 'react'
+import { usePathname } from 'next/navigation'
 
-import { useSearchStore } from '~/modules/search/store/search'
-import { Button } from '~/shared/components/Button'
-import { Input } from '~/shared/components/Input'
-import { SearchButton } from '~/shared/components/SearchButton'
 import { ThemeButton } from '~/shared/components/ThemeButton'
 import { cn } from '~/shared/utils/className'
 
+const links = [
+  {
+    href: '/favorites',
+    label: 'Favorites',
+  },
+  {
+    href: '/search',
+    label: 'Search',
+  },
+]
+
 export function Header() {
-  const router = useRouter()
   const pathname = usePathname()
-  const isFavoritesPage = pathname === '/favorites'
-
-  const { query, setQuery, isOpen, setIsOpen } = useSearchStore()
-
-  const handleOpenSearch = () => {
-    setIsOpen(!isOpen)
-  }
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    if (!query) return
-
-    event.preventDefault()
-    router.push(`/search/${query}`)
-  }
 
   return (
     <header className="relative">
@@ -38,55 +27,24 @@ export function Header() {
           <h1 className="font-serif text-3xl text-primary-foreground">
             <Link href="/">Art Explorer</Link>
           </h1>
-          <div className="flex items-center gap-2">
-            <nav>
-              <Link
-                className={cn(
-                  'rounded-md bg-transparent px-4 py-2 font-medium text-lg text-primary-foreground transition-colors duration-300 hover:bg-white',
-                  isFavoritesPage && 'bg-white',
-                )}
-                href="/favorites">
-                Favorites
-              </Link>
+          <div className="flex items-center gap-4">
+            <nav className="flex items-center gap-4">
+              {links.map((link) => (
+                <Link
+                  className={cn(
+                    'rounded-md bg-transparent px-4 py-1 font-medium text-lg text-primary-foreground transition-colors duration-300 hover:bg-white',
+                    pathname === link.href && 'bg-white',
+                  )}
+                  href={link.href}
+                  key={link.href}>
+                  {link.label}
+                </Link>
+              ))}
             </nav>
-            <SearchButton
-              isActive={isOpen}
-              onClick={handleOpenSearch}
-            />
             <ThemeButton />
           </div>
         </div>
       </div>
-      <AnimatePresence mode="wait">
-        {isOpen && (
-          <motion.div
-            animate={{ y: 0 }}
-            className="absolute inset-x-0 top-28 z-1 flex h-16 items-center bg-accent"
-            exit={{ y: -64 }}
-            initial={{ y: -64 }}
-            transition={{ duration: 0.3 }}>
-            <div className="mx-auto flex w-full max-w-7xl items-center justify-end px-4">
-              <form
-                className="relative flex w-full max-w-md items-center"
-                onSubmit={handleSubmit}>
-                <Input
-                  className="w-full"
-                  onChange={(e) => setQuery(e.target.value)}
-                  placeholder="Search for an artist, artwork, or department"
-                  value={query}
-                />
-                <Button
-                  className="absolute top-px right-px bottom-px h-auto"
-                  size="icon"
-                  type="submit"
-                  variant="ghost">
-                  <ArrowRightIcon />
-                </Button>
-              </form>
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
     </header>
   )
 }
