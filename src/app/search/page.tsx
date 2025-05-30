952796078f1617c8c3ca51de58e0d4bff25b7d3e
Adeonir Kohl <adeonir@gmail.com>
2025-05-30 20:05:34 -0300

    Update search page with dynamic metadata

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,13 @@
-import type { Metadata } from 'next'
-
 import { SearchView } from '~/modules/search/views'
 
-export const metadata: Metadata = {
-  title: 'Search | Art Explorer',
-  description: 'Search for art with the Met Museum API',
+export async function generateMetadata({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+  const params = await searchParams
+  const query = params.query
+
+  return {
+    title: `Search: ${params.query} | Art Explorer`,
+    description: `Search for art with the Met Museum API for ${query}`,
+  }
 }
 
 export default async function Page() {
