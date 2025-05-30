7f5add3f36511e31a68ef7efd28bc0e293952a70
Adeonir Kohl <adeonir@gmail.com>
2025-05-30 18:12:51 -0300

    Update metadata on details page with dynamic title

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from 'next'
 
+import { getArtwork } from '~/modules/details/actions/getArtwork'
 import { DetailsView } from '~/modules/details/views'
 
 interface PageProps {
   params: Promise<{ id: string }>
 }
 
-export const metadata: Metadata = {
-  title: 'Details | Art Explorer',
-  description: 'Details of the artwork',
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { id } = await params
+  const artwork = await getArtwork({ objectID: Number(id) })
+
+  return {
+    title: `Details of ${artwork.title} | Art Explorer`,
+  }
 }
 
 export default async function Page({ params }: PageProps) {
