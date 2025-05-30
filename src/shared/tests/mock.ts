ac525d158d2afb1d7ef2ce190e29e7b92fda3c3b
Adeonir Kohl <adeonir@gmail.com>
2025-05-30 10:10:04 -0300

    Add tests for artworks action and hook

---
+++ src/shared/tests/mock.ts
@@ -0,0 +1,11 @@
+export const mockArtwork = {
+  objectID: 1,
+  objectURL: 'https://test.com',
+  title: 'Test Artwork',
+  primaryImage: 'https://test.com',
+  primaryImageSmall: 'test-image',
+  artistDisplayName: 'Test Artist',
+  objectDate: '2023',
+  medium: 'Test Medium',
+  department: 'Test Department',
+}
