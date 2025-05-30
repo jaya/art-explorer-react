fd5d691db5918c11a30cb85a48cc01f836498442
Adeonir Kohl <adeonir@gmail.com>
2025-05-30 08:46:33 -0300

    Remove unused props from artwork interface

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,17 +2,12 @@ export interface Artwork {
   objectID: number
   objectURL: string
   title: string
-  isHighlight: boolean
   primaryImage: string
   primaryImageSmall: string
-  additionalImages: string[]
-  artistPrefix: string
   artistDisplayName: string
-  artistDisplayBio: string
   objectDate: string
   medium: string
   department: string
-  classification: string
 }
 
 export interface Department {
